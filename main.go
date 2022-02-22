d628b63b6862d6c8e80e1a3d521028033264b4bd
Dan Austin <dan.austin@hey.com>
2022-02-22 20:52:44 +0000

    feat: add endpoint to get URN details

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/djaustin/urn-lookup/urn"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	redisAddress := os.Getenv("REDIS_ADDRESS")
+	if redisAddress == "" {
+		redisAddress = "localhost:6379"
+	}
+
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(gin.Logger())
+
+	urn := urn.NewController(redisAddress)
+
+	r.GET("/urn/:id", urn.GetURN)
+
 	if err := r.Run(); err != nil {
 		log.Fatalln(err)
 	}
