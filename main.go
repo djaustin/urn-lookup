3c0b042c89030f2b4d97560d140dfc1a9ad8c070
Dan Austin <dan.austin@hey.com>
2022-02-22 17:53:39 +0000

    feat: add initial echo server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := r.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
